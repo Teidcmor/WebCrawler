a9d4830fd0cd139340c8600363d1d4a8f76bfc43
Teidcmor <exject@163.com>
2019-05-05 09:57:35 +0800

    20190505-爬取链接部分成功

--- WebCrawler/webcrawler-interface/src/main/java/common/container/HisUrlContainerUtils.java
+++ WebCrawler/webcrawler-interface/src/main/java/common/container/HisUrlContainerUtils.java
@@ -19,17 +19,16 @@ public class HisUrlContainerUtils  implements ApplicationListener {
 
     @Autowired
     private HisUrlMapper hisUrlMapper;
-    //HashMap<HisUrl.url,HisUrl.flag>
     private static final HashMap<String ,String> hisUrlContainer = new HashMap<String, String>();
 
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
-        if (((applicationEvent instanceof ContextRefreshedEvent))/* && (null == ((ContextRefreshedEvent)applicationEvent).getApplicationContext().getParent())*/ ){
+        if (((applicationEvent instanceof ContextRefreshedEvent))
+            /* && (null == ((ContextRefreshedEvent)applicationEvent).getApplicationContext().getParent())*/ ){
             initHisUrlContainer();
         }
     }
-
     /**
-     * 初始话容器
+     * 初始化容器
      */
     private void initHisUrlContainer(){
         //自动注入失败，手动注入
@@ -43,24 +42,22 @@ public class HisUrlContainerUtils  implements ApplicationListener {
             HisUrl hisUrl = (HisUrl) iterator.next();
             String url = hisUrl.getUrl();
             String flag = hisUrl.getFlag();
-            if(CommonUtils.isNull(url)){
+            if(!CommonUtils.isNull(url)){
                 hisUrlContainer.put(url,flag);
             }
         }
     }
-
     /**
      * 判断历史url容器中是否包含目标url
      * @param url
-     * @return 包含则返回true
+     * @return 缓存容器中有目标URL而且标志为0则包含返回true
      */
     public static boolean contains(String url) throws Exception{
-        String  target = null;
-        target = hisUrlContainer.get(url);
+        String target = hisUrlContainer.get(url);
         //如果url不存在或者flag不为0.则不包含，返回false
-        return target == null ? false:target=="0";
+        boolean result = target == null ? false:"0".equals(target);
+        return result;
     }
-
     /**
      * 新增历史rul缓存内容
      * @param hisUrl
@@ -69,14 +66,13 @@ public class HisUrlContainerUtils  implements ApplicationListener {
     public static void addHisUrl(HisUrl hisUrl) throws Exception{
         hisUrlContainer.put(hisUrl.getUrl(),hisUrl.getFlag());
     }
-
     /**
      * 去除指定url
      * @param hisUrl
      * @throws Exception
      */
     public static  void deleteHisUrl(HisUrl hisUrl) throws Exception{
-        hisUrlContainer.remove(hisUrl);
+        hisUrlContainer.remove(hisUrl.getUrl());
     }
 
 }
