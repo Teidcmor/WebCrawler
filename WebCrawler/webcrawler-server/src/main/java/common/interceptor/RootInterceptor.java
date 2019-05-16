e4d5edef345029d591676021663a46f2902cf531
Teidcmor <exject@163.com>
2019-05-16 22:20:13 +0800

    20190516-新增权限过滤

--- WebCrawler/webcrawler-server/src/main/java/common/interceptor/RootInterceptor.java
+++ WebCrawler/webcrawler-server/src/main/java/common/interceptor/RootInterceptor.java
@@ -1,6 +1,7 @@
 package common.interceptor;
 
 import common.pojo.UserInfo;
+import common.utils.ConstantUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import userInfo.dto.UserInfoDTO;
@@ -17,9 +18,15 @@ public class RootInterceptor implements HandlerInterceptor {
         if(requestUrl.contains("/user/login.do")||requestUrl.contains("/user/register.do"))
             return true;
         UserInfo currentUser = (UserInfo) session.getAttribute("currentUser");
+        //如果没有登录，驳回
         if(currentUser==null) {
             httpServletResponse.sendRedirect("/view/userInfo/login.jsp");
             return false;
+        }
+        //如果普通用户访问了管理员权限下的内容，驳回
+        if(requestUrl.contains("/admin")){
+            if(ConstantUtils.USER_TYPE_SIMPLE.equals(currentUser.getType()))
+                return false;
         }
         return true;
     }
